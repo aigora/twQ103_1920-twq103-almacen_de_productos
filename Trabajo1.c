f17356b822eb49e371ef5cadde2fb0da253fb4ed
WasimaSimouh <39121108+WasimaSimouh@users.noreply.github.com>
2020-05-22 20:57:30 +0200

    Add files via upload
    22.05.2020

--- Trabajo1.c
+++ Trabajo1.c
@@ -1,5 +1,6 @@
 //ALMACÉN DE PRODUCTOS
 #include<stdio.h>
+#include<string.h>
 struct persona
 {
 	char nombre1[40];
@@ -17,67 +18,83 @@ struct producto
 	float precio;
 };
 
-int main()
-{
-	int i, farmacos=0, opcion;
+void AbrirFicheroMedicamentos(struct producto farmaco[], int farmacos);
+int menu();
+void Banner();
+
+int main(){
+	system("color 4F");
+	Banner();
+	
+	struct persona contratada[7];
+	struct producto farmaco[8];
+	int i, j, farmacos=0, contratados=0, opcion, opcion1, intentos;
 	int codigoUsuario=0, existenciasUsuario=0;
 	float PrecioFinal=0.0f;
-	char despedido[40];
+	char despedido[40], jefe[7], contrasena[7], JEFE[7]="Malcom", CONTRASENA[7]="aBcDeF";
 	FILE*pfichero;
-	struct producto farmaco[8];
-	struct persona contratada;
 	
-	pfichero=fopen("fichero.txt", "r");
-            
-        if(pfichero==NULL){
-            printf("Error al abrir el fihero");
-            return 0;
-		}
-    		
-    	while (fscanf(pfichero, "%s %d %d %f", &farmaco[farmacos].nombre, &farmaco[farmacos].codigo, &farmaco[farmacos].existencias, &farmaco[farmacos].precio) != EOF){
-			farmacos++;
-		}
-
-	fclose(pfichero);
-
+	AbrirFicheroMedicamentos(farmaco, farmacos);
+	
     do
 	{ 
-	system("color 4F");
-	printf("\n                      %c%c%c%c%c MEN%c DE OPCIONES %c%c%c%c%c\n", 177, 177, 177, 177, 177, 233, 177, 177, 177, 177, 177);
-	printf("\n                        1-Trabaja con nosotros\n");
-	printf("                        2-Despedir personal\n");
-	printf("                        3-Inventario\n");
-	printf("                        4-Comprar productos\n");
-	printf("                        5-Vender productos\n");
-	printf("                        6-Con%ccenos\n", 162);
-	printf("                        7-Salir del programa\n");
-	
-	scanf("%d", &opcion);
-   
-    switch (opcion)
+	opcion1=menu(opcion);
+    switch (opcion1)
     {
     	case 1:
     		system("cls");
-    		printf("\n  Nombre: ");
-    		scanf("%s", &contratada.nombre1);
+	    	
+	    	printf("\n  Nombre: ");
+    		scanf("%s", contratada[7].nombre1);
+    		fflush(stdin);
     		printf("  Apellido: ");
-    		scanf("%s", &contratada.apellido);
+    	    scanf("%s", contratada[7].apellido);
     		printf("  G%cnero: ", 130);
-    		scanf("%s", &contratada.genero);
+    		scanf("%s", contratada[7].genero);
     		printf("  Edad: ");
-    		scanf("%d", &contratada.edad);
+    		scanf("%d", &contratada[7].edad);
     		printf("  Correo: ");
     		fflush(stdin);
-    		gets(contratada.correo);
+    		gets(contratada[7].correo);
     		printf("  N%cmero de tel%cfono: ", 163, 162);
-    		scanf("%d", &contratada.numero);
-    		printf("\n  Gracias por su inter%cs, ya le llamaremos!\n", 130);
+    		scanf("%d", &contratada[7].numero);
+	    	
+	        pfichero=fopen("empleados.txt", "a");
+	        if(pfichero==NULL){
+                printf("Error al abrir el fihero");
+                return 0;
+	        }
+    		fprintf(pfichero, "%s    %s     %s     %d    %s   %d\n", contratada[7].nombre1, contratada[7].apellido, contratada[7].genero, contratada[7].edad, contratada[7].correo, contratada[7].numero);
+			fclose(pfichero);
+    		
     		break;
+    		
     	case 2:
     		system("cls");
-    		printf("\n  Nombre del personal a despedir: ");
-    		scanf("%s", despedido);
-    		printf("\n  Usted a despedido a %s\n", despedido);
+    		intentos=3;
+    		printf("\n   --INICIAR SESI%cN-- \n", 224);
+    	    do{
+    	    	printf("\n   USUARIO JEFE: ");
+    	    	scanf("%s", jefe);
+    	    	fflush(stdin);
+    	    	printf("   CONTRASE%cA: ", 165);
+                gets(contrasena);
+    	    	intentos--;
+    	    	if((strcmp(jefe, JEFE) && strcmp(contrasena, CONTRASENA))!=0 && (intentos>0 && intentos<=3))
+    	    	    printf("   Por favor, introduzca los datos correctamente.\n");
+			}while((strcmp(jefe, JEFE) && strcmp(contrasena, CONTRASENA))!=0 && (intentos>0 && intentos<=3));
+			
+		
+			
+			if((strcmp(jefe, JEFE) && strcmp(contrasena, CONTRASENA))==0){
+				printf("\n   Nombre del personal a despedir: ");
+    	    	gets(despedido);
+    	    	
+    	    	printf("\n   Usted ha despedido a %s\n", despedido);
+			}
+			else
+			   printf("\n   **ACCESO DENEGADO**\n");
+    		
     		break;
     	case 3:
     		system("cls");
@@ -92,6 +109,7 @@ int main()
     		break;
     		
     	case 4:
+    		
     		printf("\n  Introduce el c%cdigo del producto: ", 162);
     		scanf("%d", &codigoUsuario);
     		
@@ -105,6 +123,7 @@ int main()
     		    	farmaco[i].existencias+=existenciasUsuario;
     		    }
         	}
+        	printf("\n\n*****************************TABLA ACTUALIZADA**********************************");
             printf("\n-------------------------------------------------------------------------------\n");
     	    printf("|   PRODUCTO    | CODIGO DE PRODUCTO | NUMERO DE EXISTENCIAS | PRECIO UNITARIO |\n");
     	    for (i=0; i<8; i++) {
@@ -114,6 +133,7 @@ int main()
     	    printf("-------------------------------------------------------------------------------\n");
     		break;
     	case 5:
+    		
     		printf("\n  Introduce el c%cdigo del producto: ", 162);
     		scanf("%d", &codigoUsuario);
     		
@@ -141,10 +161,46 @@ int main()
 		case 7:
     		system("cls");
     		printf("\n                           Ha salido del programa\n");
-    		break;
-    		
+            break;	
     }
-    }
-   while(opcion!=7);
+    }while(opcion1!=7);
+   return 0;
 }
+int menu(int opcion){
+	printf("\n                      %c%c%c%c%c MEN%c DE OPCIONES %c%c%c%c%c\n", 177, 177, 177, 177, 177, 233, 177, 177, 177, 177, 177);
+	printf("\n                        1-Trabaja con nosotros\n");
+	printf("                        2-Despedir personal\n");
+	printf("                        3-Inventario\n");
+	printf("                        4-Comprar productos\n");
+	printf("                        5-Vender productos\n");
+	printf("                        6-Con%ccenos\n", 162);
+	printf("                        7-Salir del programa\n");
+	
+	scanf("%d", &opcion);
+	
+	return opcion;
+}
+void Banner(){
+	printf("\n\n                *****  ******  ****** ******  ******  *\n");
+	printf("                  *    *    *  *      *    *  *    *  *\n");
+	printf("                  *    ******  ***    ******  *    *  *\n");
+	printf("                  *    *    *  *      *    *  *    *  *\n");
+	printf("                  *    *    *  ****** ******  ******  ******\n\n\n\n");
+	
+}
+void AbrirFicheroMedicamentos(struct producto farmaco[], int farmacos){
+	farmacos=0;
+	FILE*pficheros;
+	pficheros=fopen("fichero.txt", "r");
+            
+    if(pficheros==NULL){
+        printf("Error al abrir el fihero");
+	}		
+    while (fscanf(pficheros, "%s %d %d %f", &farmaco[farmacos].nombre, &farmaco[farmacos].codigo, &farmaco[farmacos].existencias, &farmaco[farmacos].precio) != EOF){
+		farmacos++;
+	}
+	fclose(pficheros);
+	
+}
+
 
